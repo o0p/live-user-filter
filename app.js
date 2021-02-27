ed9121c8f7d1602dc5952974fca30f4b5c066356
Russell <russelldev@protonmail.com>
2021-02-27 23:34:27 +0000

    Add async getData()

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const result = document.getElementById('result');
 const filter = document.getAnimations('filter');
 const listItems = [];
+
+getData();
+
+async function getData() {
+    const res = await fetch('https://randomuser.me/api?/results=50');
+    const data = await res.json();
+    console.log(data)
+}
