3932c4a1534d7e2f3471a01e634897cf31ca3a95
Russell <russelldev@protonmail.com>
2021-02-28 10:49:55 +0000

    Add more logic to  getData()

--- app.js
+++ app.js
@@ -5,7 +5,22 @@ const listItems = [];
 getData();
 
 async function getData() {
-    const res = await fetch('https://randomuser.me/api?/results=50');
-    const data = await res.json();
-    console.log(data)
+    const res = await fetch('https://randomuser.me/api?results=50');
+    const { results } = await res.json();
+
+    //clear results
+    result.innerHTML ='';
+    results.forEach(user => {
+        const li = document.createElement('li');
+        listItems.push(li);
+        li.innerHTML = `
+            <img src="${user.picture.large}" alt="${user.name.first}">
+            <div class ="user-info">
+                <h4>${user.name.first} ${user.name.last}</h4>
+                <p>${user.location.city}, ${user.location.country}</p>
+            </div>
+        `
+
+        result.appendChild(li)
+    });
 }
